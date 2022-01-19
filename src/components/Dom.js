124200d5f5f818c1ed7be3e63dea176239eb4642
Bossolin <vladyslav.okipnyak@icloud.com>
2022-01-19 17:28:08 +0100

    added visual coords

--- src/components/Dom.js
+++ src/components/Dom.js
@@ -6,13 +6,47 @@ const Dom = () => {
     header.innerText = "BATTLESHIP";
     document.body.appendChild(header);
 
+    const announcer = document.createElement("div");
+    announcer.classList.add("announcer");
+    announcer.innerText = "Place your ships";
+    document.body.appendChild(announcer);
+
+    const rotateBtn = document.createElement("button");
+    rotateBtn.innerText = "Rotate Ship";
+    announcer.appendChild(rotateBtn);
+
     const grids = document.createElement("div");
     grids.classList.add("grids");
     document.body.appendChild(grids);
 
     const generateGrid = (obj) => {
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("wrapper");
+
+      const alphabet = document.createElement("div");
+      alphabet.classList.add("alphabet");
+      wrapper.appendChild(alphabet);
+
+      for (let i = 0; i < 10; i++) {
+        const string = "ABCDEFGHIJ";
+        const letter = document.createElement("div");
+        letter.innerText = string[i];
+        alphabet.appendChild(letter);
+      }
+
+      const numbers = document.createElement("div");
+      numbers.classList.add("numbers");
+      wrapper.appendChild(numbers);
+
+      for (let i = 0; i < 10; i++) {
+        const number = document.createElement("div");
+        number.innerText = i + 1;
+        numbers.appendChild(number);
+      }
+
       const grid = document.createElement("div");
       grid.classList.add(obj.playerName);
+      wrapper.appendChild(grid);
 
       let x = 1;
       let y = 1;
@@ -30,7 +64,7 @@ const Dom = () => {
         grid.appendChild(cell);
       }
 
-      return grid;
+      return wrapper;
     };
 
     const playerGrid = generateGrid(player);
@@ -40,14 +74,16 @@ const Dom = () => {
     grids.appendChild(computerGrid);
   };
 
-  const endGame = (computerObj) => {
-    console.log("game ended");
+  const endGame = (computerObj, name) => {
+    const announcer = document.querySelector(".announcer");
 
     computerObj.forEach((cell) => {
       const newCell = cell.cloneNode(true);
 
       cell.parentNode.replaceChild(newCell, cell);
     });
+
+    announcer.innerText = `${name} won the game!`;
   };
 
   const renderShips = (playerObj, computerObj) => {
@@ -84,8 +120,10 @@ const Dom = () => {
 
         playerCell.setAttribute("class", retaliation[2]);
 
-        if (computerObj.gameboard.isFleetSunk()) endGame(computerCells);
-        if (playerObj.gameboard.isFleetSunk()) endGame(computerCells);
+        if (computerObj.gameboard.isFleetSunk())
+          endGame(computerCells, playerObj.playerName);
+        if (playerObj.gameboard.isFleetSunk())
+          endGame(computerCells, computerObj.playerName);
       };
 
       cell.addEventListener("click", checkAttack, { once: true });
